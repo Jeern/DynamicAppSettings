ec0582a6d2cd3beccc822758c33b6750dadf6884
Jesper Niedermann <jesper@niedermann.dk>
2016-07-21 21:20:45 +0200

    Now with TryValueOf + nuspec extended with projecturl and licenseurl

--- FappSettings/AppSettings.cs
+++ FappSettings/AppSettings.cs
@@ -8,12 +8,26 @@ namespace FappSettings
         {
             return func(new AppSettingValues(typeof(string), allowNull));
         }
+        public static bool TryValueOf(Func<dynamic, string> func, out string value)
+        {
+            value = func(new AppSettingValues(typeof(string), true));
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+            return true;
+        }
     }
     public static class AppSettings<T> where T : struct 
     {
         public static T ValueOf(Func<dynamic, T> func, bool allowNull = false)
         {
             return func(new AppSettingValues(typeof(T), allowNull));
+        }
+        public static bool TryValueOf(Func<dynamic, T> func, out T value)
+        {
+            value = func(new AppSettingValues(typeof(T), true));
+            if (value.IsNull())
+                return false;
+            return true;
         }
     }
 }
---
+++ FappSettings/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FappSettings
+{
+    internal static class Extensions
+    {
+        public static bool IsNull<T>(this T t)
+        {
+            return EqualityComparer<T>.Default.Equals(t, default(T));
+        }
+    }
+}
