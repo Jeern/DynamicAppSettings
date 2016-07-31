bbe5f923d2df5e26c73fc93385d070022d00522e
Jeern <jesper@niedermann.dk>
2016-07-31 22:47:24 +0200

    ConnectionStrings class for accessing connectionstrings added

---
+++ FappSettings/ConnectionStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FappSettings
+{
+    public static class ConnectionStrings
+    {
+        public static string ValueOf(Func<dynamic, string> func, bool allowNull = false)
+        {
+            return func(new ConnectionStringValues(allowNull));
+        }
+        public static bool TryValueOf(Func<dynamic, string> func, out string value)
+        {
+            value = func(new ConnectionStringValues(true));
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+            return true;
+        }
+    }
+}
