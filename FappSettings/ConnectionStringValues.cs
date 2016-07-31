b5441d0cea2d2636d4b3f9ccd1f8fb72e0222898
Jeern <jesper@niedermann.dk>
2016-07-31 22:45:50 +0200

    ConnectionStringValues added

---
+++ FappSettings/ConnectionStringValues.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Dynamic;
+
+namespace FappSettings
+{
+    internal class ConnectionStringValues : DynamicObject
+    {
+        private readonly bool _allowNull;
+
+        public ConnectionStringValues(bool allowNull)
+        {
+            _allowNull = allowNull;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string propertyName = binder.Name;
+            string connString = ConfigurationManager.ConnectionStrings[propertyName].ConnectionString;
+            if (!_allowNull && string.IsNullOrWhiteSpace(connString))
+                throw new ConfigurationErrorsException($"You need to provide a ConnectionString called {propertyName}");
+            result = connString;
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            throw new ConfigurationErrorsException("You cannot set a ConnectionString - only get is supported");
+        }
+    }
+}
